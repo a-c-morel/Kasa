215a6fc274ceabcb6cb936182ab9b000ff4c5a19
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-07-20 15:19:59 +0200

    routes: setting up the routes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Accomodation from "./pages/Accomodation"
+//import Error404 from "./components/Error404"
+
+export default function App() {
+    return (
+      <div>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/a-propos" element={<About />}/>
+            <Route path="/fiche-logement" element={<Accomodation />}/>
+        </Routes>
+      </div>
+    )
+}
