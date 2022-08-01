50b5dcb95d180e472e97dfc8a709d09eb43bae8f
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-08-01 14:55:17 +0200

    styles: info section set with flexbox

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,36 +4,41 @@ import RatingStars from './RatingStars'
 
 export default function Info({id, accomodation}) {
   return (
+    <>
     <section className="accomodation-info">
-            <h1 className="accomodation-info__title" key={id+accomodation.title.length}>{accomodation.title}</h1>
-            <p className="accomodation-info__location" key={id+accomodation.location.length}>{accomodation.location}</p>
-            <ul className="tags">
-              {accomodation.tags.map(tag => {
-                  return <li className="tag" key={tag}>{tag}</li>
-                }
-              )}
-            </ul>
-            <div className="flex-row-space-between">
-                <RatingStars rating={accomodation.rating}/>
-                <div className="identity">
-                <p className="identity__name" key={id+accomodation.host.name}>{accomodation.host.name}</p>
-                <div className="profile-img-container">
-                  <img className="profile-image" src={accomodation.host.picture} alt="profil"/>
-                </div>
-              </div>
-            </div>
-            <div className="collapses-container">
-              <Collapse collapseTitle="Description">
-                <li>{accomodation.description}</li>
-              </Collapse>
+      <div className="flex-container-first">
+        <h1 className="accomodation-info__title" key={id+accomodation.title.length}>{accomodation.title}</h1>
+        <p className="accomodation-info__location" key={id+accomodation.location.length}>{accomodation.location}</p>
+        <ul className="tags">
+          {accomodation.tags.map(tag => {
+              return <li className="tag" key={tag}>{tag}</li>
+            }
+          )}
+        </ul>
+      </div>
+      <div className="flex-container-second">
+        <RatingStars rating={accomodation.rating}/>
+        <div className="identity">
+          <p className="identity__name" key={id+accomodation.host.name}>{accomodation.host.name}</p>
+          <div className="profile-img-container">
+            <img className="profile-image" src={accomodation.host.picture} alt="profil"/>
+          </div>
+        </div>
+      </div>
+    </section>
 
-              <Collapse collapseTitle="Equipements">
-                {accomodation.equipments.map(equipment => {
-                  return <li key={id+equipment}>{equipment}</li>
-                })}
-              </Collapse>
-            </div>
-          </section>
+    <div className="collapses-container">
+      <Collapse collapseTitle="Description">
+        <li>{accomodation.description}</li>
+      </Collapse>
+
+      <Collapse collapseTitle="Equipements">
+        {accomodation.equipments.map(equipment => {
+          return <li key={id+equipment}>{equipment}</li>
+        })}
+      </Collapse>
+    </div>
+    </>
   )
 }
 
