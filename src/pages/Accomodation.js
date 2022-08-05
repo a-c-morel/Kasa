b5c02bd391ff0bed67457d11cab9181a1f7aef22
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-08-05 13:33:50 +0200

    fix: when ID doesn't exist route points to Error404

--- src/pages/Accomodation.js
+++ src/pages/Accomodation.js
@@ -3,19 +3,23 @@ import { useParams } from "react-router-dom"
 import '../index.css'
 import Gallery from "../components/Gallery"
 import Info from "../components/Info"
+import { Routes, Route } from "react-router-dom"
+import Error404 from "./Error404"
 
 export default function Accomodation({data}) {
     let {id} = useParams()
-    return (
-      <>
-        {data.filter(accomodation => accomodation.id === id).map((accomodation, id) => (
-          <div key={id}>
-            <Gallery accomodation={accomodation}/>
-            <Info id={id} accomodation={accomodation}/>
-          </div>
-        ))
-        }
-      </>
-      
+    let idMatching = data.filter(accomodation => accomodation.id === id)
+    
+    return (idMatching.length>0) ? (
+      idMatching.map((accomodation, id) => (
+        <div key={id}>
+          <Gallery accomodation={accomodation}/>
+          <Info id={id} accomodation={accomodation}/>
+        </div>
+      ))
+    ) : (
+        <Routes>
+            <Route path="*" element={<Error404 />} />
+        </Routes>
     )
 }
