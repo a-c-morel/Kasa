6863a0ec21fbaa2b40df29a06959a7d870619c31
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-08-08 09:33:11 +0200

    refactor: import data only once

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Accomodation from "./pages/Accomodation"
-import accomodations from "./assets/accomodations.json"
 import Error404 from "./pages/Error404"
 
-export default function App() {
+export default function App({data}) {
     return (
       <div>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/a-propos" element={<About />}/>
-            <Route path="/fiche-logement/:id/*" element={<Accomodation data={accomodations}/>}/>
+            <Route path="/fiche-logement/:id/*" element={<Accomodation data={data}/>}/>
             <Route path="*" element={<Error404 />} />
         </Routes>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import accomodations from './assets/accomodations.json'
 import Navbar from './components/Navbar'
 import App from './App'
 import Footer from './components/Footer'
@@ -11,7 +12,7 @@ root.render(
   <BrowserRouter>
     <div className="main-wrapper">
       <Navbar /> {/*Mettre ce composant ici évite de le répéter dans toutes les pages (idem pour le footer)*/}
-      <App className="app"/>
+      <App data={accomodations} className="app"/>
     </div>
     <Footer />
   </BrowserRouter>
